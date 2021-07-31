8549c32652e20c2b7f8ae78db26135ff6b6f936c
Camila Cristina de Mello Santos <86639088+0073010@users.noreply.github.com>
2021-07-30 21:34:56 -0300

    Update to apply the private modifier to set function.

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -1,6 +1,8 @@
 class Pessoa {
     var nome: String = "Camila"
+
     var cpf: String = "123.123.123.45"
+        private set
 
     inner class Endereço {
         var rua: String = "Rua teste"
@@ -10,7 +12,9 @@ class Pessoa {
 
 fun main() {
     val Camila = Pessoa()
+    Camila.cpf ="4567"
 
+    println(Camila)
     println(Camila.nome)
     println(Camila.cpf)
 
